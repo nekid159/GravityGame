0736d0490d51496539e1aea5089876040247a336
nekid159 <123189562+nekid159@users.noreply.github.com>
2023-10-15 00:18:02 +0300

    writing core

---
+++ my_framework/src/main/java/com/example/my_framework/SceneFW.java
@@ -0,0 +1,19 @@
+package com.example.my_framework;
+
+public abstract class SceneFW {
+    public CoreFW coreFW;
+    public int sceneWidth;
+    public int sceneHeight;
+    public GraphicsFW graphicsFW;
+
+    public SceneFW(CoreFW coreFW) {
+        this.coreFW = coreFW;
+
+    }
+
+    public abstract void update();
+    public abstract void drawing();
+    public abstract void pause();
+    public abstract void resume();
+    public abstract void dispose();
+}
